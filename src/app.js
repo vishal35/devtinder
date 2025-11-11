2baa56b7a08dd4c79d5bb4076944057e84469bed
vishal <rvishu35@gmail.com>
2025-11-11 16:35:01 +0530

    GET POST DELETE API code

--- src/app.js
+++ src/app.js
@@ -2,8 +2,26 @@ const express = require('express')
 
 const app = express()
 
-app.use('/', (req, res) => {
-    res.send('Sending from server at port 3000')
+// app.use('/', (req, res) => {
+//     res.send('Sending from server at port 3000')
+// })
+
+app.get('/user', (req, res) => {
+    res.send({firstName: 'Vishal', lastName: 'Kumar'})  
+})
+
+app.post('/user', (req, res) => {
+    // saving Data to DataBase
+    res.send('Data Suscessfully saved to DataBase');
+})
+
+app.delete('/user', (req, res) => {
+    // Deleting Data from DataBase
+    res.send('Data Suscessfully deleted from DataBase');
+})
+
+app.use('/test', (req, res) => {
+    res.send('Sending Hello from server at port 3000')
 })
 
 app.listen(3000, () => {
