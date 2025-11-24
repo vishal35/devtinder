ed2c9501f010a8ab7da73ec64d457aebb11c8eb8
vishal <rvishu35@gmail.com>
2025-11-24 18:06:21 +0530

    crud operations

--- src/app.js
+++ src/app.js
@@ -3,16 +3,84 @@ const { isAuthenticated } = require("./middlewares/auth");
 const app = express();
 const { connectDB } = require("./config/database");
 const User = require("./models/user");
+app.use(express.json());
+
+// get user for specific emailId
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    console.error("Error fetching user:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// get user by id
+app.get("/userId", async (req, res) => {
+  try{
+    const user = await User.findById(req.body.userId);
+    if(!user){
+      res.status(404).send("user not found");
+    }else{
+      res.status(200).send(user);
+    }
+  } catch(err){
+    res.status(500).send("Internal Server Error" + err.message);
+  }
+});
+
+// delete user by id
+app.delete("/user", async (req, res) => {
+  try{
+    // const user = await User.findByIdAndDelete({_id: req.body.userId}); 
+    const user = await User.findByIdAndDelete(req.body.userId); 
+    if(!user){
+      res.status(404).send("user not found");
+    } else{
+      res.status(200).send("user deleted successfully");
+    }
+  } catch(err){
+    res.status(500).send("Internal Server Error" + err.message);
+  }
+});
+
+// update user by id
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  try{
+    const user = await User.findByIdAndUpdate({_id: req.body.userId}, data); 
+    if(!user){
+      res.status(404).send("user not found");
+    } else{
+      res.status(200).send("user updated successfully");
+    }
+  } catch(err){
+    res.status(500).send("Internal Server Error" + err.message);
+  }
+});
+
+// get all users details
+app.get("/feed", async (req, res) => {
+  try{
+    const users = await User.find({});
+    if(users.length === 0){
+      res.status(404).send("no users found");
+    }else{
+      res.status(200).send(users);
+    }
+  } catch(err){
+    res.status(500).send("Internal Server Error" + err.message);
+  }
+});
 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Vishal222",
-    lastName: "Kumar",
-    emailId: "vishal@kumar.com",
-    password: "vishal@123",
-    age: "30",
-    gender: "male",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User Created Successfully");
