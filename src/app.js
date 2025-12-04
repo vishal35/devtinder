9891dff7152016b010445f870bdf20b918dfc743
vishal <rvishu35@gmail.com>
2025-12-04 13:52:59 +0530

    use login with password comapre using bcrypt compare

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { isAuthenticated } = require("./middlewares/auth");
 const app = express();
 const { connectDB } = require("./config/database");
+const bcrypt = require("bcrypt");
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validate");
 app.use(express.json());
 
 // get user for specific emailId
@@ -23,29 +25,29 @@ app.get("/user", async (req, res) => {
 
 // get user by id
 app.get("/userId", async (req, res) => {
-  try{
+  try {
     const user = await User.findById(req.body.userId);
-    if(!user){
+    if (!user) {
       res.status(404).send("user not found");
-    }else{
+    } else {
       res.status(200).send(user);
     }
-  } catch(err){
+  } catch (err) {
     res.status(500).send("Internal Server Error" + err.message);
   }
 });
 
 // delete user by id
 app.delete("/user", async (req, res) => {
-  try{
-    // const user = await User.findByIdAndDelete({_id: req.body.userId}); 
-    const user = await User.findByIdAndDelete(req.body.userId); 
-    if(!user){
+  try {
+    // const user = await User.findByIdAndDelete({_id: req.body.userId});
+    const user = await User.findByIdAndDelete(req.body.userId);
+    if (!user) {
       res.status(404).send("user not found");
-    } else{
+    } else {
       res.status(200).send("user deleted successfully");
     }
-  } catch(err){
+  } catch (err) {
     res.status(500).send("Internal Server Error" + err.message);
   }
 });
@@ -54,49 +56,90 @@ app.delete("/user", async (req, res) => {
 app.patch("/user/:id", async (req, res) => {
   const userId = req.params.id;
   const data = req.body;
-  try{
-    const ALLOWED_UPDATES = ["firstName", "lastName", "age", "gender", "photoUrl", "skills", "bio"];
-    const isAllowedUpdate = Object.keys(data).every((k)=> ALLOWED_UPDATES.includes(k));
-    if(!isAllowedUpdate){
+  try {
+    const ALLOWED_UPDATES = [
+      "firstName",
+      "lastName",
+      "age",
+      "gender",
+      "photoUrl",
+      "skills",
+      "bio",
+    ];
+    const isAllowedUpdate = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isAllowedUpdate) {
       throw new Error("invalid updates!");
     }
-    
-    if(data.skills.length > 10){
+
+    if (data.skills.length > 10) {
       throw new Error("skills exceeded the limit of 10");
     }
-    const user = await User.findByIdAndUpdate({_id: userId}, data, {runValidators: true}); 
-    if(!user){
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+    if (!user) {
       res.status(404).send("user not found");
-    } else{
+    } else {
       res.status(200).send("user updated successfully");
     }
-  } catch(err){
+  } catch (err) {
     res.status(500).send("Internal Server Error: " + err.message);
   }
 });
 
 // get all users details
 app.get("/feed", async (req, res) => {
-  try{
+  try {
     const users = await User.find({});
-    if(users.length === 0){
+    if (users.length === 0) {
       res.status(404).send("no users found");
-    }else{
+    } else {
       res.status(200).send(users);
     }
-  } catch(err){
+  } catch (err) {
     res.status(500).send("Internal Server Error" + err.message);
   }
 });
 
+app.get("/login", async (req, res) => {
+  const { emailId, password } = req.body;
+  try{
+    const user = await User.findOne({ emailId });
+    if(!user){
+      throw new Error("Invalid credentials em");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+      throw new Error("Invalid credentials pass");
+    }
+    res.status(200).send("Login Successful");
+  }catch(err){
+    res.status(500).send("Internal Server Error " + err.message);
+  }
+});
+
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
   try {
+    // validate req body
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // Encrypt password
+    const passwordHash = await bcrypt.hash(password, 10);
+    // Create new Instance of user
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    })
     await user.save();
     res.send("User Created Successfully");
   } catch (err) {
-    console.error("Error creating user:", err);
-    res.status(500).send("Internal Server Error");
+    console.error("Error creating user:", err.message);
+    res.status(500).send("Internal Server Error " + err.message);
   }
 });
 
---
+++ src/utils/validate.js
@@ -0,0 +1,17 @@
+const validator = require("validator");
+
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
+
+  if (!firstName || !lastName) {
+    throw new Error("First name and last name are required.");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Invalid email format.");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not strong enough.");
+  }
+};
+
+module.exports = {
+  validateSignUpData,
+};
